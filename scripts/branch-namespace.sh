dd06e5f5ce54c5ddc4626f6aea4ab5f62452ea64
Jacob Hrbek <kreyren@rixotstudio.cz>
2020-08-28 18:28:48 +0200

    Update branch-namespace.sh

--- scripts/branch-namespace.sh
+++ scripts/branch-namespace.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Hello it's me here again once again wheee it's meee don't hurt me"
+
 branch=`git symbolic-ref HEAD 2>&1` || error "cannot set kubectl namespace: no branch"
 currentContext=`kubectl config current-context 2>&1` || error "cannot set kubectl namespace: no current context"
 namespace=staging-`echo "$branch" | awk '{ sub(/^refs\/heads\//, ""); $0 = tolower($0); gsub(/[^-a-z0-9]/, "-"); print }'`
